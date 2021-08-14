2224ebe0378a2a7c9620042f2438b7875810ed88
ljor <leah.m.jorgensen@gmail.com>
2021-08-13 21:34:51 -0400

    generated game pieces

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const player1Board = document.querySelector('#player1-board')
-const playerGridSquares= document.querySelectorAll('#player1-board > div')
+const playerGridSquares = document.querySelectorAll('#player1-board > div')
+const pieceContainer = document.querySelector('.pieceboard')
 
 const playerSquares = []
+const gamePieces = []
 
 // Create Gameboard
 function createBoard(board, grid) {
@@ -10,18 +12,86 @@ function createBoard(board, grid) {
         square.dataset.id = i
 
         // marking edges
-        if (i <= 10 || i % 10 === 0 || i >= 91 || i % 10 === 1) {
-            square.classList.add('edge')
+        if (i <= 10) {
+            square.classList.add('top-edge')
+        }
+        if (i % 10 === 0) {
+            square.classList.add('right-edge')
+        }
+        if (i >= 91) {
+            square.classList.add('bottom-edge')
+        }
+        if (i % 10 === 1 || i === 1) {
+            square.classList.add('left-edge')
         }
 
         board.appendChild(square)
         grid.push(square)
     }
 }
-
 createBoard(player1Board, playerSquares)
 
 // Create Gamepieces
+
+function createPlayerShips() {
+    let destroyerContainer = document.createElement('div')
+    destroyerContainer.classList.add('ship', 'destroyer-container')
+    for (let i = 0; i < 2; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id', `destroyer-${i}`)
+        destroyerContainer.appendChild(square)
+    }
+    destroyerContainer.draggable = true
+    pieceContainer.appendChild(destroyerContainer)
+
+    let cruiserContainer = document.createElement('div')
+    cruiserContainer.classList.add('ship', 'cruiser-container')
+    for (let i = 0; i < 3; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id', `cruiser-${i}`)
+        cruiserContainer.appendChild(square)
+    }
+    cruiserContainer.draggable = true
+    pieceContainer.appendChild(cruiserContainer)
+
+    let submarineContainer = document.createElement('div')
+    submarineContainer.classList.add('ship', 'submarine-container')
+    for (let i = 0; i < 3; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id', `submarine-${i}`)
+        submarineContainer.appendChild(square)
+    }
+    submarineContainer.draggable = true
+    pieceContainer.appendChild(submarineContainer)
+
+    let battleshipContainer = document.createElement('div')
+    battleshipContainer.classList.add('ship', 'battleship-container')
+    for (let i = 0; i < 4; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id', `battleship-${i}`)
+        battleshipContainer.appendChild(square)
+    }
+    battleshipContainer.draggable = true
+    pieceContainer.appendChild(battleshipContainer)
+
+    let carrierContainer = document.createElement('div')
+    carrierContainer.classList.add('ship', 'carrier-container')
+    for (let i = 0; i < 5; i++){
+        let square = document.createElement('div')
+        square.setAttribute('id', `carrier-${i}`)
+        carrierContainer.appendChild(square)
+    }
+    carrierContainer.draggable = true
+    pieceContainer.appendChild(carrierContainer)
+}
+createPlayerShips()
+
+// ship piece selectors
+const destroyer = document.querySelector('.destroyer-container')
+const cruiser = document.querySelector('.cruiser-container')
+const submarine = document.querySelector('.submarine-container')
+const battleship = document.querySelector('.battleship-container')
+const carrier = document.querySelector('.carrier-container')
 
 
 // Gamepiece Placement
